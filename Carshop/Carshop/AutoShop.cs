aa7741bd67ab92840b530067ed2a4f70d3240fec
Joshua Westphalen <joshua.westphalen@yahoo.com>
2022-08-10 14:46:29 -0700

    Added intial classes for visio items

---
+++ Carshop/Carshop/AutoShop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carshop
+{
+    class AutoShop
+    {
+        private int _autoShopId;
+
+        public AutoShop(string make, string model, DateTime Year, string status)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public int autoShopId
+        {
+            get { return _autoShopId; }
+            set { _autoShopId = value; }
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime Year { get; set; }
+
+    }
+}
---
+++ Carshop/Carshop/PaymentType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carshop
+{
+    class PaymentType
+    {
+        private int _paymentTypeId;
+
+        public PaymentType(string cardType, int cardNumber, DateTime dateOfExpiration, 
+            int cvs, int authorizationNumber)
+        {
+
+        }
+
+        public int PaymentTypeId
+        {
+            get { return _paymentTypeId; }
+            set { _paymentTypeId = value; }
+        }
+
+        public string CardType { get; set; }
+
+        public int CardNumber { get; set; }
+
+        public DateTime DateOfExpiration { get; set; }
+
+        public int Cvs { get; set; }
+
+        public int AuthorizationNumber { get; set; }
+
+    }
+}
